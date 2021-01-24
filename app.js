4f2f5aa7f594aba3b4c06d127e5f64c191fd713f
Rohan Bansal <rohanbansal2567@gmail.com>
2021-01-24 18:52:28 +0530

    added Jenkinsfile

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send("Version 2 of application"))
+app.get('/', (req, res) => res.send("Version 1 of application"))
 app.listen(3000, () => console.log('Server ready'))
 
