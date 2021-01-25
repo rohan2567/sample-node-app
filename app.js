718797863bcf0ce90b526a2f2a2f335493cfe9a2
rohan2567 <57085570+rohan2567@users.noreply.github.com>
2021-01-25 14:25:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send("Version 5 of application"))
+app.get('/', (req, res) => res.send("Version 6 of application"))
 app.listen(3000, () => console.log('Server ready'))
 
